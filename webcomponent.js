69bb8d121ae4558a9e73f4938672a51e9971903c
hottoek3 <dlwl4091@gmail.com>
2025-09-04 16:31:09 +0900

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,1 +1,16 @@
-
+async loadECharts() {
+  if (!window.echarts) {
+    await new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = "https://cdn.jsdelivr.net/npm/echarts@5/dist/echarts.min.js";
+      script.onload = resolve;
+      document.head.append(script);
+    });
+    await new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = "https://cdn.jsdelivr.net/npm/echarts-gl/dist/echarts-gl.min.js";
+      script.onload = resolve;
+      document.head.append(script);
+    });
+  }
+}
